07b3cbff2341ae0761ee3904886f11bff3220ac2
99nirmit <79360867+99nirmit@users.noreply.github.com>
2025-02-04 10:56:34 +0530

    Role table created & join user & role form user_role table

---
+++ BizNest/src/main/java/com/business/BizNest/domain/Role.java
@@ -0,0 +1,30 @@
+package com.business.BizNest.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    private String roleName;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private Set<User> users;
+}
